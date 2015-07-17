04156840278a7f391e832aeca4405840ee375b79
vanan08 <vanan08@gmail.com>
2015-07-17 18:30:36 +0800

    Add get module function and constants

---
+++ keycloak-1.1.0.Beta2/services/src/main/java/org/keycloak/services/managers/Constants.java
@@ -0,0 +1,9 @@
+package org.keycloak.services.managers;
+
+/**
+Collected constants of modules name.
+*/
+public final class Constants  {
+	/** Module change password  */
+	public static final String CHANGEPASSWORD = "CHANGEPASSWORD";
+}
